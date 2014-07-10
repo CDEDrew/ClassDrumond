d5c640e52a37bd2a42f7581e85ed27de42a7e88e
madnessfranc <francoeur700@hotmail.com>
2014-07-10 14:13:22 -0400

    Add body

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
 
-	<h1>C'est normal en Russie! Luc Le Blanc AHAHA moi aussi jai acces AA</h1>
-	<h2> 1 2 3 patate poil</h2>
+	<body>
+		<h1>C'est normal en Russie! Luc Le Blanc AHAHA moi aussi jai acces AA</h1>
+		<h2> 1 2 3 patate poil</h2>
+	</body>
 </html>
